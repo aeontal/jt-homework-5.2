58e355a2134b44dada9cfc9222edfec7850c61cf
Maxim Kazantsev <aeontal@gmail.com>
2021-04-13 23:13:02 +0500

    fixed #1

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,11 +1,10 @@
 public class Main {
-  public static void main(String[] args) {
-    BonusService service = new BonusService();
-
-    long amount = 1000_60;
-    boolean registered = true;
-
-    service.calculate(amount, registered);
-  }
+    public static void main(String[] args) {
+        BonusService service = new BonusService();
+        long amount = 1000_60;
+        boolean registered = true;
+        long bonus = service.calculate(amount, registered);
+        System.out.println(bonus);
+    }
 }
 
